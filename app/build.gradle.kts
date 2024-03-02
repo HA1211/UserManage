65bc008efb361e609e6845bbcff3a801ce3ad568
HA1211 <quanghiep2408@gmail.com>
2024-03-02 19:14:11 +0700

    cm

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,8 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    implementation ("org.jetbrains.kotlin:kotlin-stdlib:1.9.0")
+
     implementation("de.hdodenhof:circleimageview:3.1.0")
 
     implementation(platform("com.google.firebase:firebase-bom:32.7.2"))
--- app/src/main/java/com/nqh/usermanage/activities/MainActivity.kt
+++ app/src/main/java/com/nqh/usermanage/activities/MainActivity.kt
@@ -1,16 +1,10 @@
 package com.nqh.usermanage.activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Layout
-import android.view.Gravity
 import android.view.MenuItem
 import android.widget.Toast
-import androidx.appcompat.widget.Toolbar
-import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.nqh.usermanage.R
@@ -19,40 +13,53 @@ import com.nqh.usermanage.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
 
+
+    lateinit var binding: ActivityMainBinding
+
+    val toolbarMain = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar_main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        setContentView(R.layout.activity_main)
         setupActionBar()
+        binding.navView.setNavigationItemSelectedListener(this)
     }
 
-    private fun setupActionBar(){
-        val toolbarMain = findViewById<Toolbar>(R.id.toolbar_main)
-        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
-         setSupportActionBar(toolbarMain)
-
+    private fun setupActionBar() {
+        setSupportActionBar(toolbarMain)
         toolbarMain.setNavigationIcon(R.drawable.ic_navigation_menu)
 
         toolbarMain.setNavigationOnClickListener {
-            if(drawerLayout.isDrawerOpen(GravityCompat.START)){
-                drawerLayout.closeDrawer(GravityCompat.START)
-            }else{
-                drawerLayout.openDrawer(GravityCompat.START)
-            }
+            toggleDrawer()
+        }
+    }
+
+    private fun toggleDrawer(){
+        if(binding.drawerLayoutMainActivity.isDrawerOpen(GravityCompat.START)){
+            binding.drawerLayoutMainActivity.closeDrawer(GravityCompat.START)
+        }else{
+            binding.drawerLayoutMainActivity.openDrawer(GravityCompat.START)
         }
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
+        when (item.itemId) {
             R.id.navigation_profile -> {
                 Toast.makeText(this@MainActivity, "Profile", Toast.LENGTH_SHORT).show()
             }
+
             R.id.navigation_sign_out -> {
                 FirebaseAuth.getInstance().signOut()
 
                 val intent = Intent(this, IntroActivity::class.java)
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+                finish()
             }
         }
+        binding.drawerLayoutMainActivity.closeDrawer(GravityCompat.START)
+        return true
     }
 }
